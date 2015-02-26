2ed4ca746183d8566b559affca8841e43d1f49d7
ohminsun <ohminsun@gmail.com>
2015-02-26 17:10:21 +0000

    fixed temp to celsius and changed case stmt to if else stmt

--- weather.rb
+++ weather.rb
@@ -8,20 +8,19 @@ def get_weather(zipcode)
 end
 
 def display_weather(temp)
-    case temp
-    when temp >= 75
+    if temp >= 24
         message = "It's hot out; drink lots of water to stay hydrated!"
-    when temp >= 60 && temp < 75
+    elsif temp >= 16 && temp < 24
         message = "I'd wear a jacket today and perhaps some sunglasses."
-    when temp >= 50 && temp < 60
+    elsif temp >= 10 && temp < 16
         message = "It's frappucino weather."
-    when temp >= 30 && temp < 50 
+    elsif temp >= 2 && temp < 10 
         message = "Getting a bit chilly out there.  Where are your mittens?"
-    when temp >= 15 && temp < 30
+    elsif temp >= 0 && temp < 2
         message = "Calling all down parkas, please find your nearest owner."
-    when temp >= 0 && temp < 15
+    elsif temp >= -5 && temp < 0
         message = "Time to retire to the South."
-    when temp < 0
+    else temp < -5
         message = "Okay, what are you even doing outside?  Stay in with a blanket and someone to cuddle with."
     end 
 end
